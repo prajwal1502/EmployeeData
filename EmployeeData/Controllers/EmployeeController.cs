1dc307d431674d5b753e2421fff9460db7ecbc37
PRAJWAL <PRAJWAL@PRAJWALDESKTOP->
2023-03-16 18:41:16 +0530

    Add project files.

---
+++ EmployeeData/Controllers/EmployeeController.cs
@@ -0,0 +1,206 @@
+﻿using EmployeeData.DAL;
+using EmployeeData.Models;
+using EmployeeData.Models.DBEntities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeData.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       private readonly EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            List<EmployeeViewModel> employeelist = new List<EmployeeViewModel>();
+
+            if (employees != null)
+            {
+                foreach(var employee in employees)
+                {
+                    var EmployeeViewModel = new EmployeeViewModel()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        DateOfBirth = employee.DateOfBirth,
+                        Email = employee.Email,
+                        Salary = employee.Salary
+                    };
+                    employeelist.Add( EmployeeViewModel );
+                }
+                return View( employeelist );
+            }
+            return View(employeelist);
+        }
+       
+        
+        
+        
+        [HttpGet]
+        public IActionResult Create( int Id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(EmployeeViewModel model)
+        {
+            try
+            { 
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        DateOfBirth = model.DateOfBirth,
+                        Email = model.Email,
+                        Salary = model.Salary
+                    };
+                    _context.Employees.Add(employee);
+                    _context.SaveChanges();
+                    TempData["succesMessage"] = "Employee created  succesfully!";
+                    return RedirectToAction("Index");
+                    }
+                else
+                {
+                    TempData["errorMessage"] = " Model data is not valid. ";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                 TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit( int Id)
+        {
+            try
+            {
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == Id);
+
+                if (employee != null)
+                {
+                    var employeeView = new EmployeeViewModel()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        DateOfBirth = employee.DateOfBirth,
+                        Email = employee.Email,
+                        Salary = employee.Salary
+                    };
+                    return View(employeeView);
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee details not available with the Id: {Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        Id = model.Id,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        DateOfBirth = model.DateOfBirth,
+                        Email = model.Email,
+                        Salary = model.Salary
+                    };
+                    _context.Employees.Update(employee);
+                    _context.SaveChanges();
+                    TempData["successMessage"] = "Employee Details updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data is invalid. ";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            try
+            {
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == Id);
+
+                if (employee != null)
+                {
+                    var employeeView = new EmployeeViewModel()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        DateOfBirth = employee.DateOfBirth,
+                        Email = employee.Email,
+                        Salary = employee.Salary
+                    };
+                    return View(employeeView);
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee details not available with the Id: {Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public IActionResult Delete(EmployeeViewModel model)
+        {
+            var employee = _context.Employees.SingleOrDefault( x => x.Id == model.Id);
+
+            try
+            {
+                if (employee != null)
+                {
+                    _context.Employees.Remove(employee);
+                    _context.SaveChanges();
+                    TempData["SuccesMessage"] = "Employee Deleted Succesfully!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee details not available with the Id: {model.Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeData/Models/EmployeeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeData.Models
+{
+    public class EmployeeViewModel
+    {   
+        public int Id { get; set; } 
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        
+        public int Salary { get; set; }
+
+        [DisplayName("Name")]
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+
+
+    }
+}
